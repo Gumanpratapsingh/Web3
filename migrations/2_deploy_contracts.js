743a67d3ee606d00c07cc16299e68d7ac369ec06
Gumanpratapsingh <gumanpratapsinghparmar@gmail.com>
2022-01-07 20:48:18 +0530

    hi

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
-//Import Contractconst DStorage = artifacts.require("DStorage");
+//Import Contract
+const DStorage = artifacts.require("DStorage");
 
-module.exports = function(deployer) {
-	//Deploy Contract
+module.exports = function (deployer) {
+	deployer.deploy(DStorage);
 };
